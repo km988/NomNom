acfb4d2c206abefeafcb87f4555d690eb234d046
km988 <kyra.malik.2010@gmail.com>
2025-07-31 11:30:58 -0700

    Update CombinationCode.py

--- CombinationCode.py
+++ CombinationCode.py
@@ -8,6 +8,7 @@ import speech_recognition as sr
 
 # Initialize the recognizer
 recognizer = sr.Recognizer()
+text = ''
 
 # Use the default system microphone as the audio source
 with sr.Microphone() as source:
@@ -21,7 +22,7 @@ with sr.Microphone() as source:
     try:
         print("Recognizing...")
         # Recognize speech using Google Web Speech API
-        text = recognizer.recognize_google(audio)
+        text = recognizer.recognize_google(audio).lower()
         print("You said:", text)
 
     except sr.UnknownValueError:
@@ -44,19 +45,21 @@ channel_servo2 = 15  # Standard servo (right arm)
 kit.servo[channel_servo1].set_pulse_width_range(500, 2500)
 kit.servo[channel_servo2].set_pulse_width_range(500, 2500)
 
-kit.continuous_servo[channel_rotation1].set_pulse_width_range(500, 2500)
+kit.servo[channel_rotation1].set_pulse_width_range(500, 2500)
 kit.continuous_servo[channel_rotation2].set_pulse_width_range(500, 2500)
 kit.continuous_servo[channel_rotation3].set_pulse_width_range(500, 2500)
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
+
+motor_speed = 50
  
 # set GPIO Pins
-GPIO_Ain1 = 11
-GPIO_Ain2 = 13
-GPIO_Apwm = 15
-GPIO_Bin1 = 29
-GPIO_Bin2 = 31
-GPIO_Bpwm = 33
+GPIO_Ain1 = 17
+GPIO_Ain2 = 27
+GPIO_Apwm = 22
+GPIO_Bin1 = 5
+GPIO_Bin2 = 6
+GPIO_Bpwm = 13
 
 # Set GPIO direction (IN / OUT)
 GPIO.setup(GPIO_Ain1, GPIO.OUT)
@@ -65,6 +68,12 @@ GPIO.setup(GPIO_Apwm, GPIO.OUT)
 GPIO.setup(GPIO_Bin1, GPIO.OUT)
 GPIO.setup(GPIO_Bin2, GPIO.OUT)
 GPIO.setup(GPIO_Bpwm, GPIO.OUT)
+
+# Both motors are stopped 
+GPIO.output(GPIO_Ain1, False)
+GPIO.output(GPIO_Ain2, False)
+GPIO.output(GPIO_Bin1, False)
+GPIO.output(GPIO_Bin2, False)
 
 # Set PWM parameters
 pwm_frequency = 1000
@@ -75,48 +84,48 @@ pwmB = GPIO.PWM(GPIO_Bpwm, pwm_frequency)
 
 # Set the duty cycle (between 0 and 100)
 # The duty cycle determines the speed of the wheels
-pwmA.start(100)
-pwmB.start(100)
+pwmA.start(0)
+pwmB.start(0)
 
 # === Helper Function ===
-def stop():
-    print("Stopping all motors")
-    kit.continuous_servo[channel_rotation1].throttle = 0
-    kit.continuous_servo[channel_rotation2].throttle = 0
-    kit.continuous_servo[channel_rotation3].throttle = 0
 
-def move_forward(speed=50):
+def move_forward(motor_speed):
     GPIO.output(GPIO_Ain1, True)
     GPIO.output(GPIO_Ain2, False)
     GPIO.output(GPIO_Bin1, True)
     GPIO.output(GPIO_Bin2, False)
-    pwmA.ChangeDutyCycle(speed)
-    pwmB.ChangeDutyCycle(speed)
+    pwmA.ChangeDutyCycle(motor_speed)
+    pwmB.ChangeDutyCycle(motor_speed)
 
-def move_backward(speed = 50):
+def move_backward(motor_speed):
     GPIO.output(GPIO_Ain1, False)
     GPIO.output(GPIO_Ain2, True)
     GPIO.output(GPIO_Bin1, False)
     GPIO.output(GPIO_Bin2, True)
-    pwmA.ChangeDutyCycle(speed)               # duty cycle between 0 and 100
-    pwmB.ChangeDutyCycle(speed)
+    pwmA.ChangeDutyCycle(motor_speed)               # duty cycle between 0 and 100
+    pwmB.ChangeDutyCycle(motor_speed)
   
-def pivot_left(speed=50):
+def pivot_left(motor_speed):
     GPIO.output(GPIO_Ain1, False)
     GPIO.output(GPIO_Ain2, True)
     GPIO.output(GPIO_Bin1, True)
     GPIO.output(GPIO_Bin2, False)
-    pwmA.ChangeDutyCycle(speed)
-    pwmB.ChangeDutyCycle(speed)
+    pwmA.ChangeDutyCycle(motor_speed)
+    pwmB.ChangeDutyCycle(motor_speed)
 
-def pivot_right(speed=50):
+def pivot_right(speed):
     GPIO.output(GPIO_Ain1, True)
     GPIO.output(GPIO_Ain2, False)
     GPIO.output(GPIO_Bin1, False)
     GPIO.output(GPIO_Bin2, True)
-    pwmA.ChangeDutyCycle(speed)
-    pwmB.ChangeDutyCycle(speed)
+    pwmA.ChangeDutyCycle(motor_speed)
+    pwmB.ChangeDutyCycle(motor_speed)
   
+channel = channel_rotation1
+angle = 45
+kit.servo[channel].angle = angle
+print ('angle: {0} \t channel: {1}'.format(angle,channel))  
+'''
 channel = channel_rotation2
 speed = 0.8
 kit.continuous_servo[channel].throttle = speed
@@ -127,17 +136,21 @@ channel = channel_rotation3
 speed = -0.8
 kit.continuous_servo[channel].throttle = speed
 time.sleep(1)
-
-move_forward()
-time.sleep(5)
+'''
+move_backward(50)
+print("Moving forward")
+time.sleep(2)
 
 if text == "crackers":
-  pivot_left()
-  time.sleep(2)
+  pivot_left(50)
+  print("Pivoting left")
+  time.sleep(0.5)
 
-if text == "Popcorn":
-  pivot_right()
-  time.sleep(2)
+elif text == "popcorn":
+  pivot_right(50)
+  print("Pivoting right")
+  time.sleep(0.5)
+ 
 
 channel = channel_servo1
 angle = 0
@@ -150,9 +163,19 @@ kit.servo[channel].angle = angle
 print ('angle: {0} \t channel: {1}'.format(angle,channel))  
 time.sleep(1)
 
-move_backwards()
-time.sleep(5)
+if text == "crackers":
+  pivot_right(50)
+  time.sleep(0.5)
 
+elif text == "popcorn":
+  pivot_left(50)
+  time.sleep(0.5)
+  
+channel = channel_rotation1
+angle = 135
+kit.servo[channel].angle = angle
+time.sleep(1)
+'''
 channel = channel_rotation2
 speed = 1
 kit.continuous_servo[channel].throttle = speed
@@ -163,7 +186,7 @@ channel = channel_rotation3
 speed = -1
 kit.continuous_servo[channel].throttle = speed
 time.sleep(1)
-
+'''
 channel = channel_servo1
 angle = 90
 kit.servo[channel].angle = angle
@@ -174,3 +197,8 @@ angle = 0
 kit.servo[channel].angle = angle
 print ('angle: {0} \t channel: {1}'.format(angle,channel))  
 time.sleep(1)
+
+move_forward(50)
+time.sleep(2)
+
+GPIO.cleanup()
